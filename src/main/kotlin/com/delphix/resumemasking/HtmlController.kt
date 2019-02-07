7a4cd849d18cfa65f5d62b1af0823660af29ec24
Derek Smart <derek.smart@delphix.com>
2019-02-06 20:52:07 -0800

    can mask demo
    Signed-off-by: Derek Smart <derek.smart@delphix.com>

--- src/main/kotlin/com/delphix/resumemasking/HtmlController.kt
+++ src/main/kotlin/com/delphix/resumemasking/HtmlController.kt
@@ -5,13 +5,45 @@ import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.http.*
 import org.springframework.web.bind.annotation.*
+import com.delphix.playground.masking.Masker
+import com.delphix.playground.masking.record.JsonRecord
 
 @RestController
 class HtmlController {
 
   @GetMapping("/")
   fun mask(@RequestBody body: String): String {
-    return "mask"
+
+    val profile: String = """operations:
+  - select:
+      name: "records[*].firstName"
+    transform:
+      method: name/english/first
+  - select:
+      name: "$..lastName"
+    transform:
+      method: name/english/last
+  - select:
+      name: "records[?(@.age > 50)].plan"
+    transform:
+      method: generate/regex
+      options:
+        expression: XXX-A
+  - select:
+      name: "records[?(@.age <= 50)].plan"
+    transform:
+      method: generate/regex
+      options:
+        expression: YYY-D
+    """
+
+    val json : JsonRecord =  JsonRecord(body)
+    var masker = Masker()
+
+    masker.startJob()
+    masker.setProfileString(profile)
+    masker.process(json)
+    return json.toJson(true)
   }
 
 }
